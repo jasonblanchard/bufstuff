cf2035be0272e45c48edde8c5ae7ef23c1a5ecac
jasonblanchard <jas.blanchard@gmail.com>
2023-02-26 20:53:21 -0500

    wip

--- client/next.config.js
+++ client/next.config.js
@@ -2,15 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@bufbuild/connect-query"],
-
-  async rewrites() {
-    return [
-      {
-        source: "/pet.v1.PetStoreService/GetPet",
-        destination: "http://localhost:8080/pet.v1.PetStoreService/GetPet",
-      },
-    ];
+  experimental: {
+    appDir: true,
   },
+
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/pet.v1.PetStoreService/GetPet",
+  //       destination: "http://localhost:8080/pet.v1.PetStoreService/GetPet",
+  //     },
+  //   ];
+  // },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
